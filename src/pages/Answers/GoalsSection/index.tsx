467bd524c26b104d59510cd6428e6581892ca43e
Elias Treadway <treadwaydesignsofficial@gmail.com>
2025-08-29 14:08:47 -0400

    Word section header changes

--- src/pages/Answers/GoalsSection/index.tsx
+++ src/pages/Answers/GoalsSection/index.tsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
 import styles from './GoalsSection.module.css'
 import wordHeader from '@/assets/images/word-header.png'
-import wordFooter from '@/assets/images/word-footer.png'
+import mwordHeader from '@/assets/images/mword-header.jpg'
 
 const GoalsSection = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 950)
+    }
+
+    checkScreenSize()
+    window.addEventListener('resize', checkScreenSize)
+
+    return () => window.removeEventListener('resize', checkScreenSize)
+  }, [])
+
   return (
     <div className={styles.container}>
-      <img src={wordHeader} alt="wordHeader" className={styles.wordHeader} />
+      <img 
+        src={isMobile ? mwordHeader : wordHeader} 
+        alt="wordHeader" 
+        className={styles.wordHeader} 
+      />
       <div className={styles.paper}></div>
       {/* <img src={wordFooter} alt="wordPages" className={styles.wordFooter} /> */}
     </div>
